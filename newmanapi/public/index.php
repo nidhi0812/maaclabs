5060c3d1660a726cfce8c14fe50a4d1397a08c50
Nidhi Singh <nidsingh@microsoft.com>
2020-04-27 23:02:03 +0530

    app.js fix

--- newmanapi/public/index.php
+++ newmanapi/public/index.php
@@ -9,10 +9,11 @@ require 'src/Token.php';
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
 print_r($_SERVER);
-if (isset($_SERVER['HTTP_ORIGIN'])) {
+if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']="34.87.91.62:81") {
     // should do a check here to match $_SERVER['HTTP_ORIGIN'] to a
     // whitelist of safe domains
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: *");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
